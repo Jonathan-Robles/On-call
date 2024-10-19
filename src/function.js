54cb9390a7dcaa5edee4c6179b2e07882d5a412b
Jonathan-Robles <jhsiete@gmail.com>
2024-10-19 02:17:48 -0500

    storedData from localstorage

--- src/function.js
+++ src/function.js
@@ -1,50 +1,47 @@
 //Array for reasons.
 const reasons = {
-  'Reason': [
-    "choose... /",
-    "Personal Matter",
-    "Illness",
-    "Late",
-    "Leave Early",
-    "Inquiry/Notes",
-    "NCNS",
-  ]
+    'Reason': ["choose... /", "Personal Matter", "Illness", "Late", "Leave Early", "Inquiry/Notes", "NCNS"]
 }
 
 
-const NAME = document.getElementById('name');
-NAME.innerHTML = textInp('Employee Name', 'text')
+const EMPLOYEEID = document.getElementById('employeeId');
+EMPLOYEEID.innerHTML =
+    container(textInp('info', 'text')) +
+    container(textInp('Cand_id', 'text')) +
+    container(textInp('Employee_Name', 'text'));
 
 div3.innerHTML += container(textInp('Scheduled Shift Date', 'date'));
 
 addButton();
 
 function container(inner) {
-  let div = document.createElement('DIV');
-  div.classList.add('container', 'main-box', 'input-group', 'input-group-sm');
-  div.innerHTML = inner;
-  return div.outerHTML;
+    let div = document.createElement('DIV');
+    div.classList.add('container', 'main-box', 'input-group', 'input-group-sm');
+    div.innerHTML = inner;
+    return div.outerHTML;
 }
 
 function textInp(item, type) {
-  let label = document.createElement('label');
-  label.setAttribute('for', item);
-  label.textContent = item;
-  let input = document.createElement('INPUT');
-  input.setAttribute('type', type);
-  input.name = item;
-  return label.outerHTML + input.outerHTML;
+    let label = document.createElement('label');
+    label.setAttribute('for', item);
+    label.textContent = item;
+    let input = document.createElement('INPUT');
+    input.setAttribute('id', item);
+    input.setAttribute('type', type);
+    input.setAttribute('class', 'form-control');
+    input.name = item;
+    return label.outerHTML + input.outerHTML;
 }
 
 function options(arr) {
-  let elements = '';
-  arr.forEach(item => {
-    const option = document.createElement('option');
-    option.setAttribute('value', item);
-    option.textContent = item;
-    elements += option.outerHTML;
-  })
-  return elements
+    let elements = '';
+    arr.forEach(item => {
+        const option = document.createElement('option');
+        option.setAttribute('value', item);
+        option.textContent = item;
+        elements += option.outerHTML;
+    })
+    return elements
 }
 
 const PRUEBA = document.getElementById('prueba');
@@ -52,68 +49,68 @@ PRUEBA.innerHTML = options(reasons.Reason);
 
 
 function selector(arr, selName) {
-  let label = document.createElement('label');
-  label.textContent = selName;
-  label.classList.add('input-group-text', 'bold');
-  const select = document.createElement('SELECT');
-  select.classList.add('class', 'form-select');
-  select.name = selName;
-  select.id = selName;
+    let label = document.createElement('label');
+    label.textContent = selName;
+    label.classList.add('input-group-text', 'bold');
+    const select = document.createElement('SELECT');
+    select.classList.add('class', 'form-select');
+    select.name = selName;
+    select.id = selName;
 
-  arr.forEach(item => {
-    const option = document.createElement('OPTION');
-    option.setAttribute('value', item);
-    option.textContent = item;
-    select.appendChild(option)
-  })
-  return label.outerHTML + select.outerHTML;
+    arr.forEach(item => {
+        const option = document.createElement('OPTION');
+        option.setAttribute('value', item);
+        option.textContent = item;
+        select.appendChild(option)
+    })
+    return label.outerHTML + select.outerHTML;
 }
 
 /// FETCHING STATIONS JSON FILE
 let x = '';
 fetch("src/states.json")
-  .then((res) => res.json())
-  .then((data) => {
-    let x = Object.values(data);
-    let abbr = [];
-    x.forEach(item => {
-      abbr.push(item.abbr + ' / ' + item.station)
-    })
-    let div2 = document.getElementById('div2');
-    div2.innerHTML += container(selector(abbr, 'Station'));
-  });
+    .then((res) => res.json())
+    .then((data) => {
+        let x = Object.values(data);
+        let abbr = [];
+        x.forEach(item => {
+            abbr.push(item.abbr + ' / ' + item.station)
+        })
+        let div2 = document.getElementById('div2');
+        div2.innerHTML += container(selector(abbr, 'Station'));
+    });
 
 
 
 fetch("src/data2.json")
-  .then((res) => res.json())
-  .then((data) => {
-    let x = "";
-    for (const index in data.radioInput) {
-      arr = data.radioInput[index];
-      console.log(data.radioInput)
+    .then((res) => res.json())
+    .then((data) => {
+        let x = "";
+        for (const index in data.radioInput) {
+            arr = data.radioInput[index];
+            console.log(data.radioInput)
 
-      x += `<div class="container main-box input-group input-group-sm">
+            x += `<div class="container main-box input-group input-group-sm">
                   <label class="input-group-text bold" for="${index}">${index}</label>`
 
-      for (let i = 0; i < arr.length; i++) {
-        x += `<div class="inp-box input-group-text">
+            for (let i = 0; i < arr.length; i++) {
+                x += `<div class="inp-box input-group-text">
               <span class="input ">${arr[i]}</span>
               <input name="${index}" value="${arr[i]}" type="radio">
               </div>`}
-      x += `</div>`
-    }
-    let div3 = document.getElementById('div3');
-    div3.innerHTML += x;
-  })
+            x += `</div>`
+        }
+        let div3 = document.getElementById('div3');
+        div3.innerHTML += x;
+    })
 
 
 function addButton() {
-  const submitBtn = document.createElement("button");
-  submitBtn.innerHTML = "Submit";
-  submitBtn.setAttribute("type", "submit");
-  submitBtn.setAttribute("class", "btn btn-primary");
-  form.innerHTML += submitBtn.outerHTML;
+    const submitBtn = document.createElement("button");
+    submitBtn.innerHTML = "Submit";
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.setAttribute("class", "btn btn-primary");
+    form.innerHTML += submitBtn.outerHTML;
 
 }
 
@@ -121,27 +118,27 @@ function addButton() {
 
 const divContainer = document.getElementsByTagName('div');
 Object.keys(divContainer).forEach((item) => {
-  divContainer[item].classList.add('container', 'main-box', 'input-group', 'input-group-sm')
+    divContainer[item].classList.add('container', 'main-box', 'input-group', 'input-group-sm')
 });
 
 
 const lblFormat = document.getElementsByTagName('label')
 Object.keys(lblFormat).forEach((item) => {
-  lblFormat[item].classList.add('input-group-text', 'bold')
+    lblFormat[item].classList.add('input-group-text', 'bold')
 });
 
-function setInputFormat(){
-  const inpFormat = document.getElementsByTagName('input')
-  Object.keys(inpFormat).forEach((item) => {
-    inpFormat[item].classList.add('form-control')
-  });
+function setInputFormat() {
+    const inpFormat = document.getElementsByTagName('input')
+    Object.keys(inpFormat).forEach((item) => {
+        inpFormat[item].classList.add('form-control')
+    });
 }
 
 //setInputFormat();
 
 const sltFormat = document.getElementsByTagName("select")
 Object.keys(sltFormat).forEach((item) => {
-  sltFormat[item].classList.add('class', 'form-select');
+    sltFormat[item].classList.add('class', 'form-select');
 });
 
 //// SELECTOR REASON LABEL SWITCHER
--- src/main.js
+++ src/main.js
@@ -1,52 +1,121 @@
 // let prueba = addOptionsToSelect
-  
+
+function convertDateToISOFormat(dateString) {
+	// Split the date string assuming it is in dd/MM/yyyy format
+	let parts = dateString.split('/');
+	// Rearrange the parts to yyyy-MM-dd
+	return `${parts[2]}-${parts[1]}-${parts[0]}`;
+}
+
+window.onload = function(){
+	
+	let dateObject = new Date;
+	let	dateStr2 = dateObject.toLocaleDateString().slice(0,10);
+	let isoDate = convertDateToISOFormat(dateStr2);
+	
+	let dateInputElement = document.getElementById('Scheduled Shift Date');
+	if(dateInputElement){
+		dateInputElement.value = isoDate;
+		
+		console.log(dateInputElement.value);
+	}else{
+		console.log(`No element found with ID: ${dateInputElement.id}`);
+	}
+}
+
+
+
+
 PRUEBA.addEventListener('change', function () {
-  if (prueba.value == "Illness" || prueba.value == "Personal Matter") {
-    prueba.previousElementSibling.textContent = "Reason for call out";
-    prueba.name = "Reason for call out";
-  } else {
-    prueba.previousElementSibling.textContent = "Reason";
-    prueba.name = "Reason";
-  }
-  console.log('hola')
+	if (prueba.value == "Illness" || prueba.value == "Personal Matter") {
+		prueba.previousElementSibling.textContent = "Reason for call out";
+		prueba.name = "Reason for call out";
+	} else {
+		prueba.previousElementSibling.textContent = "Reason";
+		prueba.name = "Reason";
+	}
+
 })
 
-let fm = document.getElementById('form');
-fm.addEventListener("submit", function (e) {
-  e.preventDefault()
 
-  const result = new FormData(form);
-  let x = [...result];
-  let information = "";
-  let additionalInfo = document.getElementById('info').value;
-  additionalInfo = additionalInfo.replaceAll("'","").replaceAll("[","").replaceAll("]","").split(',');
 
-  x.forEach( y =>{
-        information += `  ${y[0]} : ${y[1]}  <br>`;
-  });
-  information += "Notes : emptied & open shift. DPM called & notified <br>"
-  prompty.innerHTML = `${information}<br>`;
+let saleforceId = document.getElementById('Cand_id');
 
-  additionalInfo.forEach(item=>{
-    console.log(item.trim())
-  })
-const ARR = [
-    'Name',
-'Position',
-'Availability',
-'Travel/Local',
-'Organization Sector', 
-'Job Title', 
-'Facility Location'];
+saleforceId.addEventListener('change', function () {
+	let storedData = localStorage.getItem(saleforceId.value);
 
-  for(const i in additionalInfo){
-    prompty.innerHTML += `${additionalInfo[i]}<br>`;
-  }
-    let time = document.getElementById("time");
-    let id = document.getElementById("id");
-  
-    prompty.innerHTML+= `Time : ${time.value} <br>`;
-    prompty.innerHTML+= `SalesForce Id : ${id.value} <br>`
-  
+	if (storedData) {
+		let lsData = JSON.parse(storedData)
+
+		Object.keys(lsData).forEach(item => {
+			let field = document.getElementById(item);
+
+			if (field) {
+				field.value = lsData[item];
+			} else {
+				console.warn(`No form field found with ID: ${item}`);
+			}
+		});
+
+		console.log(lsData);
+	} else {
+		console.warn('No data found in localStorage for the given ID');
+	}
+
+})
+
+
+
+let formTag = document.getElementById('form');
+
+formTag.addEventListener("submit", function (e) {
+	e.preventDefault()
+	let formData = new FormData(form);
+	formData = [...formData];
+
+	let information = "";
+
+	let additionalInfo = document.getElementById('info').value;
+	additionalInfo = additionalInfo.replaceAll("'", "").replaceAll("[", "").replaceAll("]", "").split(',');
+
+	formData.forEach(y => {
+		information += `  ${y[0]} : ${y[1]}  <br>`;
+	});
+
+	let obj2 = Object.fromEntries(formData);
+
+	localStorage.setItem(obj2.Cand_id, JSON.stringify(obj2))
+
+
+
+
+
+
+	// console.log(saleforceId.value)
+
+	// console.log(localStorage[obj2.Cand_id]);
+	// console.log(saleforceId.value);
+
+
+	information += "Notes : emptied & open shift. DPM called & notified <br>"
+	prompty.innerHTML = `${information} <br>`;
+
+	const ARR = ['Name', 'Position', 'Availability', 'Travel/Local', 'Organization Sector', 'Job Title', 'Facility Location'];
+
+	// console.log(additionalInfo)
+
+	for (const i in additionalInfo) {
+		prompty.innerHTML += ` ${additionalInfo[i]} <br> `;
+	}
+
+	let time = document.getElementById("time");
+	let id = document.getElementById("id");
+
+	prompty.innerHTML += `Time : ${time.value} <br>`;
+	// prompty.innerHTML+= `SalesForce Id : ${id.value} <br>`
+
+
 
 });
+
+

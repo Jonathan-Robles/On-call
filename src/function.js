d3273b589a9412b1dc219cf800035757a0bb70e6
Jonathan-Robles <jhsiete@gmail.com>
2024-10-28 16:54:07 -0500

    get managers

--- src/function.js
+++ src/function.js
@@ -1,8 +1,8 @@
 function convertDateToISOFormat(dateString) {
-	// Split the date string assuming it is in dd/MM/yyyy format
-	let parts = dateString.split('/');
-	// Rearrange the parts to yyyy-MM-dd
-	return `${parts[2]}-${parts[0]}-${parts[1]}`;
+    // Split the date string assuming it is in dd/MM/yyyy format
+    let parts = dateString.split('/');
+    // Rearrange the parts to yyyy-MM-dd
+    return `${parts[2]}-${parts[0]}-${parts[1]}`;
 }
 // Función para filtrar las claves de localStorage según el valor proporcionado
 function filterLocalStorageByValue(searchValue) {
@@ -15,71 +15,90 @@ function filterLocalStorageByValue(searchValue) {
 }
 
 function createWdLinks(stationValue) {
-	let dateObject = new Date;
-	let firstPart = 'https://wd5.myworkday.com/loyalsource/d/app/wfs-scheduler/index$.htmld/';
-	let secondPart = '$.htmld/calendar$.htmld/week$.htmld/';
-	let date = convertDateToISOFormat(dateObject.toLocaleDateString());
-	let finalPart = '$.htmld';
+    let dateObject = new Date;
+    let firstPart = 'https://wd5.myworkday.com/loyalsource/d/app/wfs-scheduler/index$.htmld/';
+    let secondPart = '$.htmld/calendar$.htmld/week$.htmld/';
+    let date = convertDateToISOFormat(dateObject.toLocaleDateString());
+    let finalPart = '$.htmld';
 
-	const workdayLinks = [
-		{ id: "d4ba9a1fd0821001443e26c8cdb50000", name: "Alexa", sector: "YUM", sector2: "" },
-		{ id: "d4ba9a1fd0821001443e06e888ba0000", name: "Amanda", sector: "SDC", sector2: "" },
-		{ id: "ea208a98a8f61001af7295abb5b60000", name: "Brianna", sector: "DRT", sector2: "" },
-		{ id: "ea208a98a8f61001afdcf7cd4a0c0000", name: "Carolyne", sector: "ELC", sector2: "" },
-		{ id: "ea208a98a8f61001af6c999dc3830000", name: "Courtney", sector: "LRT", sector2: "" },
-		{ id: "d4ba9a1fd0821001443e20c4dff00000", name: "Emma", sector: "TCA", sector2: "" },
-		{ id: "ea208a98a8f61001afa54f416fac0000", name: "Kyle", sector: "BBT", sector2: "MIP" },
-		{ id: "d4ba9a1fd0821001443e017d780f0000", name: "Fiona", sector: "EPT", sector2: "" },
-		{ id: "d4ba9a1fd0821001443dffae7cc40002", name: "Taylor", sector: "RGV", sector2: "" }
-	];
+    const workdayLinks = [
+        { id: "d4ba9a1fd0821001443e26c8cdb50000", name: "Alexa", sector: "YUM", sector2: "" },
+        { id: "d4ba9a1fd0821001443e06e888ba0000", name: "Amanda", sector: "SDC", sector2: "" },
+        { id: "ea208a98a8f61001af7295abb5b60000", name: "Brianna", sector: "DRT", sector2: "" },
+        { id: "ea208a98a8f61001afdcf7cd4a0c0000", name: "Carolyne", sector: "ELC", sector2: "" },
+        { id: "ea208a98a8f61001af6c999dc3830000", name: "Courtney", sector: "LRT", sector2: "" },
+        { id: "d4ba9a1fd0821001443e20c4dff00000", name: "Emma", sector: "TCA", sector2: "" },
+        { id: "ea208a98a8f61001afa54f416fac0000", name: "Kyle", sector: "BBT", sector2: "MIP" },
+        { id: "d4ba9a1fd0821001443e017d780f0000", name: "Fiona", sector: "EPT", sector2: "" },
+        { id: "d4ba9a1fd0821001443dffae7cc40002", name: "Taylor", sector: "RGV", sector2: "" }
+    ];
 
-	let sectoress = stationValue.value.slice(0, 3);
-	let managerData = workdayLinks.filter(item => item.sector == sectoress || item.sector2 == sectoress);
-	let managerLink = managerData.map(item => {
-		return {
-			name: item.name,
-			link: firstPart + item.id + secondPart + date + finalPart
-		};
-	});
+    let sectoress = stationValue.value.slice(0, 3);
+    let managerData = workdayLinks.filter(item => item.sector == sectoress || item.sector2 == sectoress);
+    let managerLink = managerData.map(item => {
+        return {
+            name: item.name,
+            link: firstPart + item.id + secondPart + date + finalPart
+        };
+    });
 
-	const navElement = document.getElementById('pss-link');
-	window.open(managerLink[0].link);
-	// console.log(managerLink);
-	navElement.innerHTML = managerLink.map(item => `<a href="${item.link}" target="_blank">${item.name}</a>`).join(' ');
+    const navElement = document.getElementById('pss-link');
+    window.open(managerLink[0].link);
+    // console.log(managerLink);
+    navElement.innerHTML = managerLink.map(item => `<a href="${item.link}" target="_blank">${item.name}</a>`).join(' ');
 }
 
 function changeEventListener(inputElement) {
 
-	inputElement.addEventListener('change', function () {
-		const filteredIds = filterLocalStorageByValue(inputElement.value);
-		let storedData = localStorage.getItem(filteredIds[0]);        
-		if (storedData) {
-			let lsData = JSON.parse(storedData)
-			Object.keys(lsData).forEach(item => {
-				let field = document.getElementById(item);
-				if (field) {
-					field.value = lsData[item];
-					if (field.id == 'Station') {
-						createWdLinks(field);
-					}
-				} else {
-					console.warn(`No form field found with ID: ${item}`);
-				}
-			});
-		} else {
-			let stationElement = document.getElementById('Station');
-			if (stationElement) {
-				stationElement.addEventListener('change', () => {
-					createWdLinks(stationElement);
-				})
-			}
-			console.warn('No data found in localStorage for the given ID');
-		}
-	})
+    inputElement.addEventListener('change', function () {
+        const filteredIds = filterLocalStorageByValue(inputElement.value);
+        let storedData = localStorage.getItem(filteredIds[0]);
+        if (storedData) {
+            let lsData = JSON.parse(storedData)
+            Object.keys(lsData).forEach(item => {
+                let field = document.getElementById(item);
+                if (field) {
+                    field.value = lsData[item];
+                    if (field.id == 'Station') {
+                        createWdLinks(field);
+                    }
+                } else {
+                    console.warn(`No form field found with ID: ${item}`);
+                }
+            });
+        } else {
+            let stationElement = document.getElementById('Station');
+            if (stationElement) {
+                stationElement.addEventListener('change', () => {
+                    createWdLinks(stationElement);
+                })
+            }
+            console.warn('No data found in localStorage for the given ID');
+        }
+    })
 }
 
+function getManagerNames(stationName) {
+    if (sessionStorage.contact) {
+        const sessionData = JSON.parse(sessionStorage.contact);
 
-
+        let dateFiltered = sessionData.filter(row => row.includes(stationName))
+        const colIndexes = [6, 7, 8, 9];
+        let generatedText = "";
+        console.log('data filtered:', dateFiltered)
+        colIndexes.forEach(index => {
+            if (dateFiltered[0] !== undefined && dateFiltered[0] !== null) {
+                let name = dateFiltered[0][index];
+                if (name !== 0) {
+                    generatedText += `@${name} `;
+                }
+            }
+        });
+        return generatedText;
+    } else {
+        console.log('No data storaged')
+    }
+}
 
 const employeeInfoContainer = document.getElementById('employee-info-container');
 employeeInfoContainer.innerHTML =
@@ -111,20 +130,20 @@ function textInp(item, type) {
 }
 
 
-const callReasonOptions  = [
-    "choose... /", 
-    "Personal Matter", 
-    "Illness", "Late", 
-    "Leave Early", 
-    "Inquiry/Notes", 
-    "NCNS", 
+const callReasonOptions = [
+    "choose... /",
+    "Personal Matter",
+    "Illness", "Late",
+    "Leave Early",
+    "Inquiry/Notes",
+    "NCNS",
     "Cover a shift",
     "Hotel / Checking in/out issue",
     "Flexing EE"
 ];
 
-function createDropdownOptions(optionsArray){
-    return optionsArray.map(item => 
+function createDropdownOptions(optionsArray) {
+    return optionsArray.map(item =>
         `<option value="${item}">${item}</option>`).join('');
 }
 
@@ -149,7 +168,7 @@ function selector(arr, selName) {
     let label = document.createElement('label');
     label.textContent = selName;
     label.classList.add('input-group-text', 'bold');
-    
+
     const select = document.createElement('SELECT');
     select.classList.add('class', 'form-select');
     select.name = selName;
@@ -180,7 +199,7 @@ fetch("src/states.json")
     });
 
 
-   
+
 
 fetch("src/data2.json")
     .then((res) => res.json())
@@ -242,6 +261,10 @@ Object.keys(sltFormat).forEach((item) => {
 });
 
 //// SELECTOR REASON LABEL SWITCHER
+
+
+
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-// let stationElement = document.getElementById('Station');
+let stationElement = document.getElementById('Station');
 const saleforceId = document.getElementById('Cand_id');
 const employeeName = document.getElementById('Employee_Name');
 const formElement = document.getElementById('form');
+const excelFileElement = document.getElementById('excel-file');
 
 changeEventListener(saleforceId);
 changeEventListener(employeeName);
@@ -63,29 +64,56 @@ changeEventListener(employeeName);
 // 		console.warn('No data found in localStorage for the given ID');
 // 	}
 // })
+class Excel {
+	constructor(content) {
+		this.content = content;
+	}
+	allRows() {
+		return this.content;
+	}
+	headers() {
+		return this.content[0];
+	}
+}
 
+excelFileElement.addEventListener('change', function () {
+	readXlsxFile(excelFileElement.files[0], { sheet: 1 }).then(function (rows) {
+		const xlsx = new Excel(rows);
+		const data = xlsx.allRows();
+		sessionStorage.setItem('contact', JSON.stringify(data))
+	})
+})
 
 formElement.addEventListener("submit", function (e) {
 	e.preventDefault()
-	let formData = new FormData(form);
-	formData = [...formData];
 
+	let formData = new FormData(form);
+	console.log(formData)
+	formData = [...formData];
+	console.log("FormData as Array:", formData);
+	// const station = stationElement.value.slice(6);
+	// console.log(station)
 	let information = "";
 
 	let additionalInfo = document.getElementById('info').value;
 	additionalInfo = additionalInfo.replaceAll('"', " ").replaceAll("{", "").replaceAll("}", "").replaceAll("'", "").replaceAll("[", "").replaceAll("]", "").split(',');
 
 	formData.forEach(y => {
-		if(y[0] !=='info'){
-		information += `  ${y[0]} : ${y[1]}  <br>`;
+		if (y[0] !== 'info') {
+			information += `  ${y[0]} : ${y[1]}  <br>`;
 		}
 	});
 
-	let obj2 = Object.fromEntries(formData);
+	let formDataObj = Object.fromEntries(formData);
+	localStorage.setItem(formDataObj.Cand_id, JSON.stringify(formDataObj));
 
-	localStorage.setItem(obj2.Cand_id, JSON.stringify(obj2))
 
 	information += "Notes : emptied & open shift. DPM called & notified <br>"
+	if (formDataObj.Station) {
+		information += getManagerNames(formDataObj.Station.slice(6).trim());
+	} else {
+		console.error("Station value is undefined or empty.");
+	}
 	prompty.innerHTML = `${information} <br>`;
 
 	const ARR = ['Name', 'Position', 'Availability', 'Travel/Local', 'Organization Sector', 'Job Title', 'Facility Location'];
